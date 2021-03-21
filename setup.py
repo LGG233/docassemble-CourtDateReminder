6a940daa5c22533980f89a70cce0a8f7bcbc0fc7
Lance Godard <45862269+LGG233@users.noreply.github.com>
2021-03-20 21:56:23 -0400

    Clear out unnecessary files

--- setup.py
+++ setup.py
@@ -45,7 +45,7 @@ def find_package_data(where='.', package='', exclude=standard_exclude, exclude_d
 setup(name='docassemble.CourtDateReminder',
       version='0.0.1',
       description=('A docassemble extension.'),
-      long_description='# docassemble.CourtDateReminder\n\nA docassemble extension.\n\n## Author\n\nLance Godard, lance@thegodardgroup.com\n\n',
+      long_description='# docassemble.CourtDateReminder\r\n\r\nA docassemble extension.\r\n\r\n## Author\r\n\r\nLance Godard, lance@thegodardgroup.com\r\n\r\n',
       long_description_content_type='text/markdown',
       author='Lance Godard',
       author_email='lance@thegodardgroup.com',
